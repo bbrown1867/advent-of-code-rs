8cd7849c9fe9a9e8d266fd5e192ed0eaa8637c34
Ben Brown <ben@beninter.net>
2022-12-02 12:11:40 -0500

    Refactor to use traits, smarter maths

--- year2022/src/day02.rs
+++ year2022/src/day02.rs
@@ -1,99 +1,122 @@
 use super::text_file_to_vec;
-
-const SHAPE_ROCK: u32 = 1;
-const SHAPE_PAPER: u32 = 2;
-const SHAPE_SCISSORS: u32 = 3;
-const RESULT_WIN: u32 = 6;
-const RESULT_LOSS: u32 = 0;
-const RESULT_DRAW: u32 = 3;
+use std::str::FromStr;
 
 pub fn main() {
     let raw_data = text_file_to_vec("data/day02.txt");
 
-    let score = score_strategy_guide_part1(&raw_data);
+    let score = score_strategy_guide(&raw_data, true);
     println!("day02 part1: {}", score);
 
-    let score = score_strategy_guide_part2(&raw_data);
+    let score = score_strategy_guide(&raw_data, false);
     println!("day02 part2: {}", score);
 }
 
-fn get_shape(symbol: &str) -> u32 {
-    match symbol {
-        "A" | "X" => SHAPE_ROCK,
-        "B" | "Y" => SHAPE_PAPER,
-        "C" | "Z" => SHAPE_SCISSORS,
-        _ => panic!("Invalid input"),
+type Score = u32;
+
+#[derive(Clone, Copy, PartialEq)]
+enum Shape {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+#[derive(Clone, Copy, PartialEq)]
+enum Outcome {
+    Win,
+    Loss,
+    Draw,
+}
+
+impl Shape {
+    fn new(val: Score) -> Self {
+        match val {
+            1 => Shape::Rock,
+            2 => Shape::Paper,
+            3 => Shape::Scissors,
+            // Trick to allow subtracting 1 to work when finding losing shape
+            0 => Shape::Scissors,
+            _ => panic!("Invalid value"),
+        }
+    }
+
+    fn value(&self) -> Score {
+        match &self {
+            Shape::Rock => 1,
+            Shape::Paper => 2,
+            Shape::Scissors => 3,
+        }
     }
 }
 
-fn get_outcome(symbol: &str) -> u32 {
-    match symbol {
-        "X" => RESULT_LOSS,
-        "Y" => RESULT_DRAW,
-        "Z" => RESULT_WIN,
-        _ => panic!("Invalid input"),
+impl FromStr for Shape {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "A" | "X" => Ok(Shape::Rock),
+            "B" | "Y" => Ok(Shape::Paper),
+            "C" | "Z" => Ok(Shape::Scissors),
+            _ => Err(()),
+        }
     }
 }
 
-fn score_hand(player: u32, opponent: u32) -> u32 {
-    player
+impl Outcome {
+    fn value(&self) -> Score {
+        match &self {
+            Outcome::Win => 6,
+            Outcome::Loss => 0,
+            Outcome::Draw => 3,
+        }
+    }
+}
+
+impl FromStr for Outcome {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "X" => Ok(Outcome::Loss),
+            "Y" => Ok(Outcome::Draw),
+            "Z" => Ok(Outcome::Win),
+            _ => Err(()),
+        }
+    }
+}
+
+fn score_hand(player: Shape, opponent: Shape) -> Score {
+    player.value()
         + if player == opponent {
-            RESULT_DRAW
-        } else if (player == SHAPE_ROCK && opponent == SHAPE_SCISSORS)
-            || (player == SHAPE_PAPER && opponent == SHAPE_ROCK)
-            || (player == SHAPE_SCISSORS && opponent == SHAPE_PAPER)
-        {
-            RESULT_WIN
+            Outcome::Draw.value()
+        } else if ((player.value() % 3) + 1) == opponent.value() {
+            Outcome::Loss.value()
         } else {
-            RESULT_LOSS
+            Outcome::Win.value()
         }
 }
 
-fn find_hand(outcome: u32, opponent: u32) -> u32 {
-    if outcome == RESULT_DRAW {
+fn find_hand(outcome: Outcome, opponent: Shape) -> Shape {
+    if outcome == Outcome::Draw {
         opponent
-    } else if outcome == RESULT_WIN {
-        match opponent {
-            SHAPE_ROCK => SHAPE_PAPER,
-            SHAPE_PAPER => SHAPE_SCISSORS,
-            SHAPE_SCISSORS => SHAPE_ROCK,
-            _ => panic!("Invalid input"),
-        }
+    } else if outcome == Outcome::Win {
+        Shape::new((opponent.value() % 3) + 1)
     } else {
-        match opponent {
-            SHAPE_ROCK => SHAPE_SCISSORS,
-            SHAPE_PAPER => SHAPE_ROCK,
-            SHAPE_SCISSORS => SHAPE_PAPER,
-            _ => panic!("Invalid input"),
-        }
+        Shape::new(opponent.value() - 1)
     }
 }
 
-fn score_strategy_guide_part1(strategy_guide: &Vec<String>) -> u32 {
+fn score_strategy_guide(strategy_guide: &Vec<String>, part1: bool) -> Score {
     let mut score = 0;
     for line in strategy_guide {
         if line.len() == 0 {
             break;
         }
         let mut split = line.split_whitespace();
-        let opponent = get_shape(split.next().unwrap());
-        let player = get_shape(split.next().unwrap());
-        score += score_hand(player, opponent);
-    }
-
-    score
-}
-
-fn score_strategy_guide_part2(strategy_guide: &Vec<String>) -> u32 {
-    let mut score = 0;
-    for line in strategy_guide {
-        if line.len() == 0 {
-            break;
-        }
-        let mut split = line.split_whitespace();
-        let opponent = get_shape(split.next().unwrap());
-        let outcome = get_outcome(split.next().unwrap());
-        let player = find_hand(outcome, opponent);
+        let opponent = Shape::from_str(split.next().unwrap()).unwrap();
+        let player = if part1 {
+            Shape::from_str(split.next().unwrap()).unwrap()
+        } else {
+            let outcome = Outcome::from_str(split.next().unwrap()).unwrap();
+            find_hand(outcome, opponent)
+        };
         score += score_hand(player, opponent);
     }
 
@@ -113,11 +136,11 @@ mod test {
 
     #[test]
     fn test_score_strategy_guide_part1() {
-        assert_eq!(score_strategy_guide_part1(&get_test_input()), 15);
+        assert_eq!(score_strategy_guide(&get_test_input(), true), 15);
     }
 
     #[test]
     fn test_score_strategy_guide_part2() {
-        assert_eq!(score_strategy_guide_part2(&get_test_input()), 12);
+        assert_eq!(score_strategy_guide(&get_test_input(), false), 12);
     }
 }
